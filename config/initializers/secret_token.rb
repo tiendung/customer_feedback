dde57b8295324438119f0def8bd22570068b6d4a
Alex N <alex@vinova.sg>
2010-09-24 06:46:11 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CustomerFeedback::Application.config.secret_token = 'a7a43a01071a636f792d5cb9d1239d8bf8765f5586dbc712b35dc7e423d54f317a908204f02a4368952abb949db3139be14218244e839c972d87480783f399e7'
---
+++ db/migrate/20100923234258_create_feedbacks.rb
@@ -0,0 +1,15 @@
+class CreateFeedbacks < ActiveRecord::Migration
+  def self.up
+    create_table :feedbacks do |t|
+      t.string :name
+      t.text :content
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :feedbacks
+  end
+end
